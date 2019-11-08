7d1486c2c2566e80a8e13bceb4b60bccdbd6038a
Warren Gates <warrenckhan@gmail.com>
2019-11-08 16:53:42 -0500

    created Header and reformatted pages into smart components || class components

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,20 @@
-export default ()=>(
-  <div>
-    kappa
-  </div>
-)
+import React, { Component } from 'react';
+
+export default class extends Component {
+	static getInitialProps({ query: { about } }) {
+		
+		return { about: about };
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>My blog post #{this.props.about}</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</p>
+			</div>
+		);
+	}
+}
--- pages/code/index.js
+++ pages/code/index.js
@@ -1,5 +1,17 @@
-export default ()=>(
-  <div>
-    kappa
-  </div>
-)
+import React, { Component } from 'react';
+
+export default class extends Component {
+	static getInitialProps() {
+		const isServer = typeof window === 'undefined';
+		return { isServer };
+	}
+
+	render() {
+		return (
+			<div>
+        <h1>Code</h1>
+				<strong>{this.props.isServer ? 'Server' : 'Client'} side</strong>.
+			</div>
+		);
+	}
+}

38e0bdba25be5a715cb5d500b78111bcea13ac41
Warren Gates <warrenckhan@gmail.com>
2019-11-08 23:23:23 -0500

    github page now gets data sent from backend to be displayed on frontend

--- pages/github/index.js
+++ pages/github/index.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-
+ 
 export default class extends Component {
-	static getInitialProps() {
-		const isServer = typeof window === 'undefined';
-		return { isServer };
+	static getInitialProps({query}) {
+		console.log(query);
+			const {
+				login,
+				avatarUrl,
+				bio,
+				isHireable,
+				isBountyHunter,
+				organizations,
+				starredRepositories,
+				watching
+			} = query.githubData
+		return {
+			login: login,
+			avatarUrl: avatarUrl,
+			bio: bio,
+			isHireable: isHireable,
+			isBountyHunter: isBountyHunter,
+			organizations: organizations,
+			starredRepositories: starredRepositories,
+			watching: watching
+		};
 	}
 
 	render() {
+		const {
+			login,
+			avatarUrl,
+			bio,
+			isHireable,
+			isBountyHunter,
+			organizations,
+			starredRepositories,
+			watching
+		} = this.props;
 		return (
 			<div>
 				<h1>Github</h1>
-				<strong>{this.props.isServer ? 'Server' : 'Client'} side</strong>.
+				<p className='lead'>
+					<strong>{login} side</strong>
+				</p>
+				.
 			</div>
 		);
 	}

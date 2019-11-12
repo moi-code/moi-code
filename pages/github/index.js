cb0d93e0351f5e1cf1e5e023a0ffb4d861677456
Warren Gates <warrenckhan@gmail.com>
2019-11-12 17:11:37 -0500

    created live page for twitch stream and hosts

--- pages/github/index.js
+++ pages/github/index.js
@@ -1,73 +1,20 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
 
 export default class extends Component {
-	static async getInitialProps({ req }) {
-		var config = {
-			headers: { Authorization: 'bearer ' + process.env.GITHUB_TOKEN }
+	static async getInitialProps({ query }) {
+		return {
+			login: query.githubData.login,
+			avatarUrl: query.githubData.avatarUrl,
+			bio: query.githubData.bio,
+			isHireable: query.githubData.isHireable,
+			isBountyHunter: query.githubData.isBountyHunter,
+			organizations: query.githubData.organizations,
+			starredRepositories: query.githubData.starredRepositories,
+			watching: query.githubData.watching
 		};
-		const q = `{
-  viewer {
-    login
-    avatarUrl
-    bio
-    isHireable
-    isBountyHunter
-    organizations(last: 4) {
-      edges {
-        node {
-          name
-          url
-        }
-      }
-    }
-    starredRepositories(last: 3) {
-      edges {
-        node {
-          name
-          description
-          url
-        }
-      }
-    }
-    watching(last: 3) {
-      edges {
-        node {
-          name
-          description
-          url
-        }
-      }
-    }
-  }
-}
-
-`;
-		const v = `{
-    }
-		`;
-		let resp;
-		Axios.post(
-			'https://api.github.com/graphql',
-			{ query: q, variables: v },
-			config
-		)
-			.then(resp => {
-				const queryParams = {
-					githubData: resp.data.data.viewer
-				};
-
-				return queryParams;
-			})
-			.catch(err => {
-				console.log('Error:', err);
-			});
 	}
-
 	render() {
-		const { queryParams } = this.props;
-
-		const {
+const {
 			login,
 			avatarUrl,
 			bio,
@@ -76,59 +23,62 @@ export default class extends Component {
 			organizations,
 			starredRepositories,
 			watching
-		} = queryParams.githubData;
+		}=this.props
 		return (
-			<div className='container mx-auto'>
-				<h1>Github Profile</h1>
+      <div className="container mx-auto my-5 p-3 border border-dark">
+        <div className="p-3 my-2">
+          <img
+            className="p-2 border border-dark"
+            src={avatarUrl}
+            alt="Github Profile Avatar"
+          />
+        <h5 className='mt-2'>
+          <a href={`https:www.github.com/${login}`}>@{login}</a>
+        </h5>
+        </div>
 
-				<div className='p-3 my-2'>
-					<img src={avatarUrl} alt='Github Profile Avatar' />
-				</div>
-
-				<p className='lead'>
-					<a href={`https:www.github.com/${login}`}>@{login}</a>
-				</p>
-				<p>
-					Hey, I'm a Mobile and Full-Stack Web Developer slowly mastering the
-					various elements that make up my stack. For Front-End I Primarily use
-					For Back-End I tend to use Express for my webserver paired with
-					NodeJs, MongoDB or Google's Cloud Firestore for my Database.
-				</p>
-				<p>{bio}</p>
-				<p>Hireable: {isHireable.toString().toUpperCase()}</p>
-				<div className='d-flex flex-column flex-md-row mt-5 justify-content-center align-items-center'>
-					<div className='col-md d-flex flex-column align-items-center'>
-						<h6>My Organizations</h6>
-						<ul>
-							{organizations.edges.map((org, i) => (
-								<li key={i}>
-									<a href={org.node.url}>{org.node.name.toUpperCase()}</a>
-								</li>
-							))}
-						</ul>
-					</div>
-					<div className='col-md d-flex flex-column align-items-center'>
-						<h6>Recently Starred</h6>
-						<ul>
-							{starredRepositories.edges.map((star, i) => (
-								<li key={i}>
-									<a href={star.node.url}>{star.node.name.toUpperCase()}</a>
-								</li>
-							))}
-						</ul>
-					</div>
-					<div className='col-md d-flex flex-column align-items-center'>
-						<h6>Latest Watched</h6>
-						<ul>
-							{watching.edges.map((watch, i) => (
-								<li key={i}>
-									<a href={watch.node.url}>{watch.node.name.toUpperCase()}</a>
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
-			</div>
-		);
+        <p>
+          Hey, I'm a Mobile and Full-Stack Web Developer slowly mastering the
+          various elements that make up my stack. For Front-End I Primarily use
+          For Back-End I tend to use Express for my webserver paired with
+          NodeJs, MongoDB or Google's Cloud Firestore for my Database.
+        </p>
+        <p>{bio}</p>
+        <p>Hireable: {isHireable.toString().toUpperCase()}</p>
+        <p>Bounty Hunter: {isBountyHunter.toString().toUpperCase()}</p>
+        <div className="d-flex flex-column flex-md-row mt-5 justify-content-between align-items-start">
+          <div className="col-md d-flex flex-column justify-content-center align-items-start">
+            <h6 className="text-left">My Organizations</h6>
+            <ul>
+              {organizations.edges.map((org, i) => (
+                <li key={i}>
+                  <a href={org.node.url}>{org.node.name.toUpperCase()}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-md d-flex flex-column align-items-start">
+            <h6 className="text-left">Recently Starred</h6>
+            <ul>
+              {starredRepositories.edges.map((star, i) => (
+                <li key={i}>
+                  <a href={star.node.url}>{star.node.name.toUpperCase()}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-md d-flex flex-column align-items-start">
+            <h6 className="text-left">Latest Watched</h6>
+            <ul>
+              {watching.edges.map((watch, i) => (
+                <li key={i}>
+                  <a href={watch.node.url}>{watch.node.name.toUpperCase()}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
 	}
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,52 +1,65 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+const buttonClick = e => {
+  e.preventDefault();
+  document.querySelector(".collapse").classList.contains("show")
+    ? document.querySelector(".collapse").classList.remove("show")
+    : document.querySelector(".collapse").classList.add("show");
+};
 
-const links = [
-	{ href: 'https://zeit.co/now', label: 'ZEIT' },
-	{ href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-	link.key = `nav-link-${link.href}-${link.label}`;
-	return link;
-});
-
+const isActive = () => {
+  document.querySelectorAll(".nav-item").forEach(el => {
+    if (window.location.pathname == el.href) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+};
 const Nav = () => (
-	<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-		<Link href='/'>
-			<a className='navbar-brand'>Moi Code</a>
-		</Link>
-		<button
-			className='navbar-toggler'
-			type='button'
-			data-toggle='collapse'
-			data-target='#moiNavbar'
-			aria-controls='moiNavbar'
-			aria-expanded='false'
-			aria-label='Toggle navigation'>
-			<span className='navbar-toggler-icon'></span>
-		</button>
-		<div className='collapse navbar-collapse' id='moiNavbar'>
-			<ul className='navbar-nav mr-auto'>
-				<li className='nav-item active'>
-					<Link href='/'>
-						<a className='nav-link'>
-							Home <span className='sr-only'>(current)</span>
-						</a>
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<a className='nav-link' href='/github'>
-						Moi Git
-					</a>
-				</li>
-				<li className='nav-item'>
-					<a className='nav-link' href='/live'>
-						Live
-					</a>
-				</li>
-			</ul>
-			<span className='navbar-text'>Navbar text with an inline element</span>
-		</div>
-	</nav>
+  <span>
+    <nav className="sticky-top navbar navbar-expand-lg navbar-light bg-light">
+      <Link href="/">
+        <a className="navbar-brand">Moi Code</a>
+      </Link>
+      <button
+        onClick={buttonClick}
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#moiNavbar"
+        aria-controls="moiNavbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="moiNavbar">
+        <ul className="navbar-nav mr-auto">
+          <li onClick={isActive} className="nav-item">
+            <Link href="/">
+              <a className="nav-link">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </Link>
+          </li>
+          <li onClick={isActive} className="nav-item">
+            <a className="nav-link" href="/github">
+              Moi-Git
+            </a>
+          </li>
+          <li onClick={isActive} className="nav-item">
+            <a className="nav-link" href="/live">
+              Live
+            </a>
+          </li>
+        </ul>
+        <span className="navbar-text">
+          {"<"}Where Creation Happens{">"}
+        </span>
+      </div>
+    </nav>
+  </span>
 );
 
 export default Nav;

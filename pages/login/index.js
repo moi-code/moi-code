3f8367c1d84a12dfe8274d5a1fb4aedf929799be
Warren Gates <warrenckhan@gmail.com>
2019-11-13 21:17:48 -0500

    added loh

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,14 +1,43 @@
-import React,{ Component } from 'react';
-
+import React, { Component } from "react";
+import './login.css'
 class Login extends Component {
+  onTextChange(e){
+    console.log()
+  }
+  render() {
+    return (
+      <div className="h-100 form mx-auto d-flex flex-column justify-content-center">
+        <form className="mt-5 p-5 mx-auto border border-dark bg-white">
 
-  render(){
-    return(
-      <div>
-        Login
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              onChange={}
+              value="password"
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;

9fd6e29554f2e71d0f98a5a9bb9f78444027a038
Warren Gates <warrenckhan@gmail.com>
2019-11-15 16:12:17 -0500

    fixed inputs updating information on Redux

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,37 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './login.css';
-import { MoiInput } from '../../src/components/common';
-import { loginTextChange } from './../../src/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./login.css";
+import { MoiInput } from "../../src/components/common";
+import { loginTextChange } from "./../../src/actions";
 class Login extends Component {
+
   render() {
-    const { email, password, loginTextChange } = this.props;
+    const { email, password, loginTextChange, disabled } = this.props;
     return (
-      <div className='h-100 form mx-auto d-flex flex-column justify-content-center'>
-        <form className='mt-5 p-5 mx-auto border border-dark bg-white'>
+      <div className="h-100 form mx-auto d-flex flex-column justify-content-center">
+        <form onSubmit={(e)=>{
+          e.preventDefault()
+          console.log('Logging In',email,password)
+        }} className="mt-5 p-5 mx-auto border border-dark bg-white">
           <MoiInput
-            label='Email'
-            onChangeText={value =>
-              loginTextChange({ prop: 'email', value })
-            }
+            label="Email"
+            onChangeText={e => {
+              e.preventDefault();
+              loginTextChange({
+                prop: "email",
+                value: e.target.value
+              });
+            }}
             value={email}
-            type='email'
-            className='form-control'
-            id='loginEmail'
-            placeholder='Email@Email.com'
+            disabled={disabled}
+            type="email"
+            className="form-control"
+            id="loginEmail"
+            placeholder="Email@Email.com"
           />
           <MoiInput
-            label='Password'
-            onChangeText={value =>
-              loginTextChange({ prop: 'email', value })
-            }
+            label="Password"
+            onChangeText={e => {
+              e.preventDefault();
+              loginTextChange({
+                prop: "email",
+                value: e.target.value
+              });
+            }}
             value={password}
-            type='password'
-            className='form-control'
-            id='loginPassword'
-            placeholder='Password'
+            type="password"
+            className="form-control"
+            id="loginPassword"
+            placeholder="Password"
           />
-          <button type='submit' className='btn btn-primary'>
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
--- src/actions/AuthActions/index.js
+++ src/actions/AuthActions/index.js
@@ -1,3 +1,5 @@
+import types from "../types";
+const { SET_LOGIN_TEXT } = types;
 export const loginTextChange = payload => dispatch => {
   dispatch({
     type: SET_LOGIN_TEXT,
